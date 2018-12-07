795b4faa3f34cb01cc425928012bafd53d1a20e8
Chase <cparker0820@gmail.com>
2018-12-06 19:30:03 -0500

    Variable Mutation Practice

--- 2-Basics/script.js
+++ 2-Basics/script.js
@@ -1,3 +1,8 @@
+/**********************
+Variables and Data Types
+*/
+
+/*
 var firstName = 'John';
 console.log(firstName);
 
@@ -12,3 +17,29 @@ console.log(job);
 
 job = 'Teacher';
 console.log(job);
+*/
+
+/************************************
+Variables Mutation and Type Coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//Variable Mutation
+age = 'twenty eight';
+job = 'driver';
+
+//'alert' creates a popup that must be cleared before continuing
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
